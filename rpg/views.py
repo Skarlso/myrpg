ef7f177e683b2274e643a18062ec1573051793ab
Gergely <hannibal@Defaults-MacBook-Pro.local>
2015-04-10 10:13:06 +0200

    Created skeleton project, added default login view, setup CSRF on login.

---
+++ rpg/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
+from django.template.context_processors import csrf
+from django.template import RequestContext
+
+def login_user(request):
+    c = {}
+    c.update(csrf(request))
+    state = "Please log in below..."
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+            else:
+                state = "Your account is not active, please contact the site admin."
+        else:
+            state = "Your username and/or password were incorrect."
+
+    return render_to_response('auth.html',{'state':state, 'username': username}, context_instance=RequestContext(request))
