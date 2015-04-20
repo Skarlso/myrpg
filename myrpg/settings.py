d7574ee62686caf4b6515e8ccaeef412a3c313dd
Gergely Brautigam <skarlso777@gmail.com>
2015-04-20 08:31:37 +0200

    Had to change app from myrpg to rpg in order for the templating to be picked up.

--- myrpg/settings.py
+++ myrpg/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myrpg',
+    'rpg',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'myrpg.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['rpg/templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
